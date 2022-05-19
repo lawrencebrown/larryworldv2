fef3733fcb3601ef8c4cf9c32a6f42de36c875c9
Lawrence Brown <email@lawrencebrown.eu>
2022-05-19 06:29:06 +0100

    style and 404
    Added in basic 404. Button hover css

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@
  */
 
 get_header(); ?>
-
+<content>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
@@ -16,45 +16,19 @@ get_header(); ?>
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'larryworld' ); ?></h1>
 				</header><!-- .page-header -->
+				
+				
+					<img src="<?php bloginfo('template_directory'); ?>/images/peaches-404.jpg" />
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'larryworld' ); ?></p>
+					
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'larryworld' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'larryworld' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+</content>
 <?php
 get_footer();
